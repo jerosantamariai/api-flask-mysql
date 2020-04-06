2f2b2561141e51fd86e1dab571c3a3ab19728c90
Jeronimo Santa Maria I <jerosantamariai@gmail.com>
2020-04-06 16:59:46 -0400

    get is ready

--- app.py
+++ app.py
@@ -4,41 +4,57 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
 from models import db
 from family import Family
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@ipserver/database'
-
 db.init_app(app)
 Migrate(app, db)
 CORS(app)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand) # init, migrate, upgrade
-fam = Family('Rodriguez')
-
+fam = Family('Doe')
 @app.route('/')
 def root():
     return render_template('index.html')
-
 @app.route('/family', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @app.route('/family/<int:id>', methods=['GET','PUT', 'DELETE'])
-def family():
+def family(id = None):
     if request.method == 'GET':
-        pass
+        if id is not None:
+            member = fam.get_member(id) # saco todos los members segun id
+            if member:
+                return jsonify(member), 200
+            else:
+                return jsonify({"msg": "Not Found"}), 404
+        else:
+            members = fam.get_all_members() # devuelve todos los members
+            return jsonify(members), 200
     if request.method == 'POST':
-        pass
+        
+        name = request.json.get("name", None)
+        age = request.json.get("age", None)
+        lucky_number = request.json.get("lucky_number", None)
+        
+        if not name and name == "":
+            return jsonify({"msg": "Field name is required"}), 400  # 400 o 422
+        if not age and age == "":
+            return jsonify({"msg": "Field age is required"}), 400  # 400 o 422
+        if not lucky_number and lucky_number == "":
+            return jsonify({"msg": "Field lucky_number is required"}), 400  # 400 o 422
+            
+        member = fam.add_member({"name": name, "age": age, "lucky_number": lucky_number}) # make a new member
+        return jsonify(member), 201
+
     if request.method == 'PUT':
         pass
     if request.method == 'DELETE':
         pass
-
 @manager.command
 def hello():
     "Just say hello"
     print("hello")
-
 if __name__ == '__main__':
     manager.run()
--- family.py
+++ family.py
@@ -1,17 +1,60 @@
+from random import randint
+
 class Family:
+
     def __init__(self, last_name):
         self._last_name = last_name
         self._members = []
+        # self._name = ""
+        self._age = 0
+        self._lucky_number = []
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        pass
+        member["id"] = self._generateId() 
+        member["last_name"] = self._last_name
+        # member["_age"] = self._age
+        # member["_lucky_number"] = self._lucky_number
+        self._members.append(member)
+        return member
+
     def delete_member(self, id):
+        # member["id"] = self._get_member()
         pass
+
     def update_member(self, id, member):
+        # member["id"] = self._get_member()
         pass
+
     def get_member(self, id):
-        pass
-    def get_all_members(self, id):
+        member = list(filter(lambda member: member if member["id"] == id else None, self._members)) #filtre member y me de el member segun id si exite
+        return member
+
+    def get_all_members(self):
         return self._members
+
+
+        # MEMBERS[
+        #     MEMBER1{
+        #         id:
+        #         last name:
+        #         name:
+        #         age:
+        #         lucky_number
+        #     },
+        #     MEMBER2{
+        #         id:
+        #         last name:
+        #         name:
+        #         age:
+        #         lucky_number
+        #     },
+        #     MEMBER3{
+        #         id:
+        #         last name:
+        #         name:
+        #         age:
+        #         lucky_number
+        #     }
+        # ]
